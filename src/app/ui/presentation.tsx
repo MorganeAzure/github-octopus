331670cec83791071856cb78bf6fd8b9844bd731
xavierpicciotto <morgane.picciotto@outlook.com>
2024-05-25 12:23:21 +0200

    hover scale for cards

--- src/app/ui/presentation.tsx
+++ src/app/ui/presentation.tsx
@@ -13,22 +13,22 @@ export default function Presentaion(){
               <h2 className="text-xl font-semibold text-white mb-6">SHE-EO ・ ENTREPRENEUR</h2>
             </div>
             <div className="grid grid-cols-2 gap-4 m-auto w-full max-w-4xl">
-              <div className="bg-teal-400 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-teal-400 hover:scale-105 p-8 text-white text-center rounded-lg shadow-lg">
                 Download My Book
               </div>
-              <div className="bg-pink-400 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-pink-400 hover:scale-105 p-8 text-white text-center rounded-lg shadow-lg">
                 10 Online Side Hustles
               </div>
-              <div className="bg-purple-500 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-purple-500 hover:scale-105 p-8 text-white text-center rounded-lg shadow-lg">
                 15 Money Saving Ideas
               </div>
-              <div className="bg-orange-400 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-orange-400 hover:scale-105 p-8 text-white text-center rounded-lg shadow-lg">
                 How To Be A Good Mentor
               </div>
-              <div className="bg-indigo-600 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-indigo-600 hover:scale-105 p-8 text-white text-center rounded-lg shadow-lg">
                 Future Proofing Your Career
               </div>
-              <div className="bg-yellow-400 p-8 text-white text-center rounded-lg shadow-lg">
+              <div className="bg-yellow-400 hover:scale-105  p-8 text-white text-center rounded-lg shadow-lg">
                 40 Simple Self Care Tips
               </div>
             </div>
